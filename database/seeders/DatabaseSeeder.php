e497d65541bddbd704f891300bef4b1ba21069cc
ZoeDoit <138300621+ZoeDoit@users.noreply.github.com>
2023-12-09 21:47:38 +0900

    シーダー作成（levels、types、prefectures）

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,9 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call([
             UsersSeeder::class,
+            LevelsSeeder::class,
+            TypesSeeder::class,
+            PrefecturesSeeder::class,
             ]);
     }
 }
---
+++ database/seeders/TypesSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TypesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('types')->insert([
+            [
+                'type_name' => '観光',
+            ],
+            [
+                'type_name' => '体験',
+            ],
+            [
+                'type_name' => '芸術',
+            ],
+            [
+                'type_name' => '歴史',
+            ],
+            [
+                'type_name' => '自然',
+            ],
+            [
+                'type_name' => '食事',
+            ],
+            [
+                'type_name' => '買い物',
+            ],
+            [
+                'type_name' => '宿泊',
+            ],
+            [
+                'type_name' => 'その他',
+            ],
+        ]);
+    }
+}
